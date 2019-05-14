cbaa152cb4b048861644829bcae39472ca33101e
Rob <rd.trammel@gmail.com>
2019-05-14 12:58:33 -0500

    Thought it'd be fun to try and add sticky nav

--- cit160/assignments/js/index.js
+++ cit160/assignments/js/index.js
@@ -1,3 +1,21 @@
-var dateEl = document.querySelector('.todays-date');
+document.querySelector('.todays-date').innerHTML = new Date().toLocaleDateString();
 
-dateEl.innerHTML = new Date().toLocaleDateString();
+function returnToTop(){
+    window.scroll({ top:0, behavior: "smooth" })
+}
+
+let header = document.querySelector("header");
+let mainContent = document.querySelector(".main-content");
+
+//Add stickytop scrolling? For fun?
+window.addEventListener('scroll', ()=>{ 
+    if (this.scrollY > 15){
+        header.addEventListener("click", returnToTop, true);
+        header.classList.add("sticky");
+        mainContent.classList.add("augment-top");
+    } else {
+        header.classList.remove("sticky");
+        mainContent.classList.remove("augment-top");
+        header.removeEventListener("click", returnToTop, true);
+    }
+});
