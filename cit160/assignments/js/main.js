ad988613892da2ebc8e2fc612d6e6aeb81580a6c
Rob <rd.trammel@gmail.com>
2019-05-15 13:07:33 -0500

    Can't stop playing with the css

--- cit160/assignments/js/main.js
+++ cit160/assignments/js/main.js
@@ -1,14 +1,21 @@
 document.querySelector('.todays-date').innerHTML = new Date().toLocaleDateString();
 
+let header = document.querySelector("header");
+let mainContent = document.querySelector(".main-content");
+let footerLabel = document.querySelector("footer h2");
+
+//Add stickytop scrolling? For fun?
+window.addEventListener('scroll', ()=>{ setHeightListeners(); setWidthListeners();});
+
+//Shrink text when the window width is small. (For phones etc.)
+window.addEventListener('resize', ()=>{ setWidthListeners();});
+
+//Functions
 function returnToTop(){
     window.scroll({ top:0, behavior: "smooth" })
 }
 
-let header = document.querySelector("header");
-let mainContent = document.querySelector(".main-content");
-
-//Add stickytop scrolling? For fun?
-window.addEventListener('scroll', ()=>{ 
+function setHeightListeners(){
     if (this.scrollY > 15){
         header.addEventListener("click", returnToTop, true);
         header.addEventListener("touchend", returnToTop, true);
@@ -20,4 +27,14 @@ window.addEventListener('scroll', ()=>{
         header.removeEventListener("click", returnToTop, true);
         header.removeEventListener("touchend", returnToTop, true);
     }
-});
+}
+
+function setWidthListeners(){
+    if (window.innerWidth < 600){
+        footerLabel.classList.add("small-text");
+        header.classList.add("small-text")
+    } else {
+        footerLabel.classList.remove("small-text");
+        header.classList.remove("small-text")
+    }
+}
