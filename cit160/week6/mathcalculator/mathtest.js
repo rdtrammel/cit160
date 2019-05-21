62128b551f99f00d53d53944ea2f163a9d3e5100
Robert Trammel <rd.trammel@gmail.com>
2019-05-20 22:42:25 -0500

    Adding Math Calculator Assignment

---
+++ cit160/week6/mathcalculator/mathtest.js
@@ -0,0 +1,5 @@
+document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString();
+
+/*
+The program should allow a child to enter two numbers and an arithmetic operator (+, −, *, or /) and the child’s answer to the problem. The program should compute the correct answer and compare it to the child’s answer and output “Correct! Good job.” or “Incorrect. Try again!”
+*/
