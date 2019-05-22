f51d473ff709dc15ce6a5cd61ee3696da510acb9
Rob <rd.trammel@gmail.com>
2019-05-22 17:38:55 -0500

    Committing roman numeral work.

--- cit160/week6/roman-numerals/romannumerals.js
+++ cit160/week6/roman-numerals/romannumerals.js
@@ -18,18 +18,30 @@ C	    100
 D	    500
 M	    1000
 */
+document.getElementById("arabic-number").addEventListener("change", whenInRome );
 
 function whenInRome(){
-    let number = parseInt(document.getElementById("arabic-number").value);
+    let output="";
+    let number = String(document.getElementById("arabic-number").value).split("");
     //Every 5, you're going to change the leading numeral
-    //On 5, you'll have both.
-    let ones, tens, hundreds, thousands;
-    if ( number < 4 ) { 
-        for ( let i = 0 ; i > 3 ; i++){ones+= "I"} 
-        ones +="V";
-    } else if (number > 4) {
-        ones +="V";
-        for ( let i = 0 ; i > 3 ; i++ ){ ones+="I"}
+    let ones = parseInt(number[number.length-1]);
+    let tens = parseInt(number[number.length-2]);
+    let hundreds = parseInt(number[number.length-3]);
+    let thousands = parseInt(number[number.length-4]);
+    thousands = thousands > 3 ? 3 : thousands;
+    if (thousands) output += getNumerals("thousands", thousands, ["M"]);
+    if (hundreds) output += getNumerals("hundreds", hundreds, ["C", "D", "M"]);
+    if (tens) output += getNumerals("tens", tens, ["X", "L", "C"]);
+    if (ones) output += getNumerals("ones", ones, ["I", "V", "X"]);
+    document.getElementById("output").innerHTML = output;
+}
+
+function getNumerals(type, num, chars){
+    let output = "";
+    if (num < 5){
+        if ( num == 4 ){ output = `${chars[0]}${chars[1]}` }else{ for (let i = 0 ; i < num && i < 3 ; i++){ output += chars[0];} }
+    } else {
+        if (num === 9){ output=`${chars[0]}${chars[2]}`}else{output = chars[1]; for (let i = 0; i < num - 5; i++){output += chars[0];}} 
     }
-    document.getElementById("output").innerHTML = ones;
+    return output;
 }
