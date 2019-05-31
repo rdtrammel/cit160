1f1ace254d72aac2b5717553ea18d127b85ed788
Rob <rd.trammel@gmail.com>
2019-05-31 13:22:36 -0500

    polishing up cashier app

--- cit160/week7/cashier/cashier.js
+++ cit160/week7/cashier/cashier.js
@@ -7,29 +7,23 @@ Finish by adding 6% sales tax to the balance. It looks like days are 0 based. So
 
 function checkOut(){
     let subtotal = parseFloat(document.getElementById("subtotal").value);
-    //let today = new Date().getDay();
-    let today = 2
-    let discount = 0;
-    if (subtotal > 50){
-        if (today == 2 || today == 3){
-            discount = subtotal * .1;
-        }
-    }
+    let today = new Date().getDay();
+    //let today = 2 //uncomment to simulate Wednesday for testing discount
+    let discount = subtotal > 50 && (today == 2 || today == 3) ? subtotal * .1 : 0;
+    //Using toFixed converted to a string, so I wrapped the whole thing in a parseFloat.
     let salesTax = parseFloat(((subtotal - discount) * .06).toFixed(2));
     document.getElementById("output").innerHTML = 
         `<div class="checkout-table">
             <table>
                 <tr>
-                    <td>Subtotal</td>
                     <td>Discount(10%)</td>
                     <td>Tax(6%)</td>
                     <td>Total</td>
                 </tr>
                 <tr>
-                    <td>$${subtotal}</td>
                     <td>$${discount}</td>
                     <td>$${salesTax}</td>
-                    <td>$${(subtotal - discount)+salesTax}</td>
+                    <td>$${parseFloat(((subtotal - discount)+salesTax).toFixed(2))}</td>
                 </tr>
             </table>
         </div>
