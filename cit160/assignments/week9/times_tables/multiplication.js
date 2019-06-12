192f21851b59597cd96b687b9b87d14d01857759
Rob <rd.trammel@gmail.com>
2019-06-12 14:11:06 -0500

    committing multiplication table app

---
+++ cit160/assignments/week9/times_tables/multiplication.js
@@ -0,0 +1,13 @@
+document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString();
+
+/* Output the times table from 1 to 12 for any positive integer that the USER INPUTS from 1 to 10 inclusive. */
+
+function getMultiplicationTable() {
+    console.log("am i working?");
+    let num = parseInt(document.getElementById("input").value);
+    let output = "";
+    for ( let i = 1 ; i <= 12 ; i++ ){
+        output += `${num} x ${i} = ${num*i}<br>`;
+    }
+    document.getElementById("output").innerHTML = output;
+}
