df14f9289ef634c93a5a8ebdad7b3cdd629dbaa4
Rob <rd.trammel@gmail.com>
2019-06-18 15:30:05 -0500

    Cleaning up Javascript

--- cit160/assignments/week9/oldest_temple/oldtemple.js
+++ cit160/assignments/week9/oldest_temple/oldtemple.js
@@ -3,9 +3,8 @@ document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString
 /* Output the oldest, running temple of The Church from a limited list provided in an HTML data table */
 
 let table = document.getElementById("templeData");
-let maxYear = [...table.rows].sort((a,b)=>{return b-a;})[1].cells[1].innerHTML;
-let minYear = parseInt(maxYear);
-let minRow = new String();
+let minYear = parseInt(table.rows[1].cells[1].innerHTML);
+let minRow;
 
 [...table.rows].forEach(row=>{
     let dedicationYear = parseInt(row.cells[1].innerHTML);
@@ -16,8 +15,7 @@ let minRow = new String();
 });
 
 document.getElementById("output").innerHTML =   
-`
-<h3>Oldest Operating Temple</h3>
+`<h3>Oldest Operating Temple</h3>
 <table class="output-data">
     <tr>
         <th>Temple</th>
