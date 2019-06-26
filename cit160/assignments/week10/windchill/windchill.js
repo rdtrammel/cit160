2c28e6cc9de3666e7cdfb776305011cbd09d929b
Robert Trammel <rd.trammel@gmail.com>
2019-06-25 20:39:44 -0500

    Cleaning up windchill app

--- cit160/assignments/week10/windchill/windchill.js
+++ cit160/assignments/week10/windchill/windchill.js
@@ -2,25 +2,21 @@ document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString
 
 let tempEl = document.getElementById("temp");
 let windEl = document.getElementById("wind");
-let chillEl = document.getElementById("chill");
-
 tempEl.addEventListener("keyup", doInputOutput);
 windEl.addEventListener("keyup", doInputOutput);
 
+/*Your program will calculate the windchill factor given a current temperature and current wind speed as inputs. We will be working in Fahrenheit (F) units. 'The wind chill explains how cold people and animals feel when outside. It is based on the amount and rate of heat lost from skin exposed to wind and cold.'*/
+
 function doInputOutput(){
     let chill = windChill(parseFloat(tempEl.value), parseFloat(windEl.value));
-    if (chill < -200) chill = "☠";
-    chillEl.value = chill;
+    document.getElementById("chill").value = chill;
 }
 
 function windChill(tempF, speed){
-    let maxTemp = 50, minSpeed = 3;
-    if((tempF > maxTemp) || (speed < minSpeed) || (!tempF && tempF != 0) || !speed ) return "N/A";
-    // f = 35.74 + .6125f - 35.75s^.16 + .4275fs^.16
-    let chill = 
-        35.74 + 
-        ( .6215 * tempF ) - 
-        ( 35.75 * Math.pow(speed, .16)) + 
-        ( .4275 * (tempF * Math.pow(speed, .16)));
-    return Math.round(chill);
+    let maxTemp = 50, minSpeed = 3, chill = "N/A";
+    if((tempF <= maxTemp) && (speed >= minSpeed)){
+        // f = 35.74 + .6125f - 35.75s^.16 + .4275fs^.16
+        chill = Math.round(35.74 + ( .6215 * tempF ) - ( 35.75 * Math.pow(speed, .16)) + ( .4275 * (tempF * Math.pow(speed, .16))));
+    }
+    return chill;
 }
