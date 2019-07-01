ab6707371fbf1b0ab7bc06f20b20efa1053842a2
Robert Trammel <rd.trammel@gmail.com>
2019-07-01 13:27:55 -0500

    Committing Week 11 doFV Assignment

--- cit160/assignments/week11/investments/investments.js
+++ cit160/assignments/week11/investments/investments.js
@@ -8,25 +8,28 @@ document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString
     d) calls the computeFutureValue function
     e) displays the result to the user
 */
-doFV();
+
 function doFV(){
     let principal = parseFloat(document.getElementById("principal").value);
     let rate = parseFloat(document.getElementById("rate").value);
     let years = parseInt(document.getElementById("years").value);
     let periods = parseInt(document.getElementById("periods").value);
-    document.getElementById("output").value = computeFutureValue(principal,rate,years,periods);
+    document.getElementById("output").value = '$' + computeFutureValue(principal, rate , years, periods);
 }
 /*
 2) function computeFutureValue ()
     a) Computes and returns the future value of an investment. 
     b) The formula for computing the future value of an investment is:
-        f = a (1 + r)n
+        f = a (1 + r)^n
         f is the future value
         a is the investment amount sometimes called the principal 
         r is the growth rate per period
         n is the total number of periods throughout the life of the investment. 
 */
 
-function computeFutureValue(principal, annualRate, years, periods){
-    return principal * (1 + (annualRate/periods)) * (periods*years);
+function computeFutureValue(principal, annualRate, years, periodsPerYear){
+    let periodRate = annualRate/periodsPerYear;
+    let totalPeriods = periodsPerYear*years;
+    let f = principal * Math.pow((1 + periodRate), totalPeriods);
+    return f.toFixed(2);
 }
