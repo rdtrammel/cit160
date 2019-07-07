2b5c374aa1dd51d01a648adb8251505ecdf3f41d
Robert Trammel <rd.trammel@gmail.com>
2019-07-07 17:14:49 -0500

    pushing week 13 assignment to assignment portal

---
+++ cit160/assignments/week13/arraysii/arraysii.js
@@ -0,0 +1,59 @@
+document.getElementById('currentdate').innerHTML = new Date().toLocaleDateString();
+
+function countEvens(list) {
+    //counts and returns the number of even integers in an array
+    let count = 0;
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] % 2 === 0) count++;
+    }
+    return count;
+}
+
+function multiply(list, multiplier) {
+    //multiply each item in the array by a given multiplier
+    return list.map(x => {
+        return x *= multiplier
+    });
+}
+
+function doTests(count) {
+    let output = "";
+    generateTestArrays(count).forEach(test => {
+        let multiplier = randomNumber(10, 1);
+        output +=
+            `<tr>
+            <td>[${test}]</td>
+            <td>${countEvens(test)}</td>
+            <td>Multiplier(${multiplier}) [${multiply(test, multiplier)}]</td>
+        </tr>`;
+    });
+    document.getElementById('output').innerHTML =
+        `<table class="results">
+        <tr>
+            <th>Tests</th>
+            <th>countEvens(list)</th>
+            <th>multiply(list, multiplier)</th>
+        </tr>
+        ${output}
+    </table><br>`;
+}
+
+function generateTestArrays(count) {
+    let output = [];
+    let countOfTests = count || 3;
+    for (let i = 0; i < countOfTests; i++) {
+        let test = [];
+        let testLn = randomNumber(7, 3);
+        for (let n = 0; n < testLn; n++) {
+            test.push(randomNumber(10));
+        }
+        output.push(test);
+    }
+    return output;
+}
+
+function randomNumber(max, min) {
+    let x = max || 10; //Default max is 10
+    let y = min || 0; //Default min is 0
+    return Math.floor(Math.random() * (x - y) + y);
+}
