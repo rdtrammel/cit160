a04e6bee7e0567ef440270e32bf8dab9f0e00bba
rdtrammel <rd.trammel@gmail.com>
2019-10-10 08:29:07 -0500

    tweaking date formatting

--- cit230/lesson4/js/preston-4.js
+++ cit230/lesson4/js/preston-4.js
@@ -2,7 +2,7 @@ function toggleMenu(){
     document.getElementById("primary-nav").classList.toggle("hide");
 }
 
-function myDateFormat(date){
+function myDateFormat(date){ 
     let dayName = new Intl.DateTimeFormat('en-US', {weekday:"long"}).format(date);
     let monthName = new Intl.DateTimeFormat('en-US', { month:"long" }).format(date);
     return `${dayName}, ${date.getDay()} ${monthName} ${date.getFullYear()}`;
