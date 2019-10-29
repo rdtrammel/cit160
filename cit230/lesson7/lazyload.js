e70bc70382299994903fc8d26ffc61185d75b0f4
Robert Trammel <rd.trammel@gmail.com>
2019-10-28 22:30:38 -0500

    committing progressive image loading module

---
+++ cit230/lesson7/lazyload.js
@@ -0,0 +1,26 @@
+const images = document.querySelectorAll("[data-src]");
+
+console.log(images);
+
+const imgOptions={
+    threshold : 0,
+    rootMargin : "0px 0px 200px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver)=>{
+    entries.forEach(entry=>{
+        if (!entry.isIntersecting) return;
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+    });
+}, imgOptions);
+
+images.forEach(image=>{
+    imgObserver.observe(image);
+})
+
+function preloadImage(img){
+    let src = img.getAttribute('data-src');
+    if (!src) return;
+    img.src = src;
+}
