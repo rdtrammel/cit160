86fb1985a70852439bf43fc8d0c9714020fba101
rdtrammel <rd.trammel@gmail.com>
2019-11-05 10:28:46 -0600

    Cleaning up Storm Center Form

--- cit230/weatherAssets/js/weather-center.js
+++ cit230/weatherAssets/js/weather-center.js
@@ -1,9 +1,13 @@
 setTopNavigation(); //Calls a script that will render the top site navigation
 setLazyLoad("[data-src]"); //setLazyLoad(selector)
 const stormDate = document.querySelector("[name=stormDate]");
-const severitySlider = document.querySelector("");
+const severitySlider = document.querySelector("[type=slider]");
+
+severitySlider.addEventListener('change',()=>{
+    
+});
+
 let today = new Date();
 let day = `0${today.getDate()}`;
-
 //Set default Storm Report date to today, format must be yyyy-mm-dd
 stormDate.value = `${today.getFullYear()}-${today.getMonth()}-${day.substring(day.length-2,day.length)}`;
