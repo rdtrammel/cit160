03ce14e0cee2b7082519b6b8c5d9517d8a82c9c3
Robert Trammel <rd.trammel@gmail.com>
2019-11-30 22:25:45 -0600

    Cleaning up styling of home page

--- cit230/waboveground/js/main.js
+++ cit230/waboveground/js/main.js
@@ -13,23 +13,31 @@ function renderWeatherData(obj){
     
     //Set up the variable to build the html
     let html = new String();
+
     obj.towns.forEach(town=>{
         if ( town.name.match(/Preston/gi) || town.name.match(/Fish/gi) || town.name.match(/Soda/gi)){
+            let eventHTML = new String();
+            town.events.forEach(event=>{
+                eventHTML += `<li>${event}</li>`;
+            });
+
             html += `
                 <div class="card">
                     <section class="information">
                         <h2>${town.name}</h2>
-                        <h4 class="motto">${town.motto}</h4>
-                        <p class="info">Founded: ${town.yearFounded}</p>
-                        <p class="info">Population: ${town.currentPopulation}</p>
-                        <p class="info">Average Rain: ${town.averageRainfall} ft</p>
-                    </section>
-                    <section class="image">
                         <picture>
                             <source srcset="../images/${town.photo}_800.jpg" media="(min-width:600px) and (max-width:999px)">
                             <source srcset="../images/${town.photo}_1200.jpg" media="(min-width:1000px)">
                             <img src="../images/${town.photo}_500.jpg" alt="Picture of ${town.name}">
                         </picture>
+                        <h4 class="motto">${town.motto}</h4>
+                        <p class="info">Founded: ${town.yearFounded}</p>
+                        <p class="info">Population: ${town.currentPopulation}</p>
+                        <p class="info">Average Rain: ${town.averageRainfall} ft</p>
+                    </section>
+                    <section class="events">
+                        <h2>Upcoming Events</h2>
+                        <ul>${eventHTML}</ul>
                     </section>
                 </div>
             `;
