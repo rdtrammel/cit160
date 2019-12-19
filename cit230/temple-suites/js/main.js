666e5051edbf0c1079089fd4628aeff186998ada
rdtrammel <rd.trammel@gmail.com>
2019-12-19 09:35:14 -0600

    committing vanilla routing structure

--- cit230/temple-suites/js/main.js
+++ cit230/temple-suites/js/main.js
@@ -1,1 +1,24 @@
 WebFont.load({ google: { families: [ 'Raleway: 300,400', 'Baskervville' ] } });
+
+const app = document.getElementById("app");
+const nav = document.querySelectorAll("#top-nav ul li a");
+
+nav.forEach(item=>{
+    item.addEventListener("click", ()=>{loadPage(item.getAttribute('data-href'));});
+});
+
+//default route on loading index is main.html
+let route = "pages/main.html";
+loadPage(route);
+
+//Function used to load the page snippets into the main view
+function loadPage(route){
+    fetch(route)
+    .then(response=> response.text())
+    .then(html=>{app.innerHTML = html;});
+}
+
+//Function used to toggle the Navigation Menu in smaller views. Default is hidden
+function toggleMenu(){
+    document.querySelector("#top-nav ul").classList.toggle("hidden");
+}
